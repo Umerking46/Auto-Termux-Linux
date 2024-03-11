82252adac9cace7c79ad3d910410019fa05ef9ae
Umerking46 <129172390+Umerking46@users.noreply.github.com>
2024-03-11 23:01:05 +0500

    Auto-Termux
    Auto-Termux is a simple bash script which automatically install all the basic packages you need to be installed in your termux this is my first script so please tell me if you have any problem with this script

---
+++ auto-termux.sh
@@ -0,0 +1,47 @@
+To modify your bash script to display "This Script is written by Mr Zade 2.0" in green and with a bigger font size, you can use special formatting options. Here's an updated version of your script:
+
+```bash
+#!/bin/bash
+
+echo -e "\e[92m\033[5mThis Script is written by Mr Zade 2.0\e[0m"
+pkg update -y
+pkg upgrade -y
+pkg install python -y
+pkg install python2 -y
+pkg install fish -y
+pkg install ruby -y
+pkg install git -y
+pkg install php -y
+pkg install perl -y
+pkg install nmap -y
+pkg install bash -y
+pkg install clang -y
+pkg install nano -y
+pkg install w3m -y
+pkg install hydra -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install unzip -y
+pkg install tor -y
+pkg install wget -y
+pkg install wcalc -y
+pkg install bmon -y
+pkg install golang -y
+pkg install openssl -y
+pkg install cmatrix -y
+pkg install openssh -y
+pkg install wireshark -y
+pkg install toilet -y
+pkg install sl -y
+pkg install vim -y
+pkg install tch -y
+pkg install zsh -y
+pkg install fortune -y
+pkg install zsh -y
+apt update -y
+apt upgrade -y
+echo -e "\e[92m\033[5mThis Script is written by Mr Zade 2.0\e[0m"
+echo -e "\033[0;31mCongratulations, you are done! Now you can use your Termux.\033[0m"
